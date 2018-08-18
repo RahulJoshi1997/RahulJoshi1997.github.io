f3afce4145ab99a6324cfbb9648b45a6f2bb34f7
Rahul Joshi <>
2018-08-18 14:30:58 +0530

    Completed the pop-up image viewer

--- scripts/slideShow.js
+++ scripts/slideShow.js
@@ -1,30 +1,8 @@
-function openSlideShow() {
-  document.getElementById("slideShow").style.display = "block";
+function openSlide(img) {
+    document.getElementById("slideShow").style.display = "block";
+    document.getElementById("slide").style.backgroundImage = "url('../" + img + "')";
 }
 
-function close() {
-  document.getElementById("slideShow").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  
-  slides[slideIndex-1].style.display = "block";
+function closeSlide() {
+    document.getElementById("slideShow").style.display = "none";
 }
