208840b3452e61d3be03320d77f3de913066f496
Rahul Joshi <>
2018-08-25 03:06:41 +0530

    Added content

---
+++ scripts/headerContent.js
@@ -0,0 +1,18 @@
+function printHeader (pathPrefix) {
+    var headerString = '\
+    <header class="clearfix">\
+        <!-- Title -->\
+        <a href="' + pathPrefix + 'index.html"><div id="title">\
+            <h1>Rahul Joshi</h1>\
+            <h2>I design and develop games</h2>\
+        </div></a>\
+        <!-- Contact Me -->\
+        <p id="contactMe">\
+            <a href="mailto:rj1093@nyu.edu">rj1093@nyu.edu</a><br />\
+            <a href="' + pathPrefix + 'aboutMe/index.html">About Me</a><br />\
+            <a href="' + pathPrefix + 'aboutMe/RahulJoshi_Resume.pdf">Resume</a>\
+        </p>\
+    </header>';
+
+    document.body.insertAdjacentHTML ( 'afterbegin', headerString);
+}
